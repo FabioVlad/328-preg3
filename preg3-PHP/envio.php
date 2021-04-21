b141f4a7ac62908a22538556cb778ca6535960bc
FabioVlad <81887721+FabioVlad@users.noreply.github.com>
2021-04-20 22:37:56 -0400

    Add files via upload

---
+++ preg3-PHP/envio.php
@@ -0,0 +1,12 @@
+<form method="GET" action="http://localhost:8080/WebApplication2/NewServlet">
+  Nombre<input type="text" name="nombre"/></br>	
+  Apellido<input type="text" name="apellido"/></br>
+  $hoy = getdate();
+  <input type="submit" name="Enviar a NetBeans">
+</form>
+
+<form method="GET" action="http://localhost:18320/Default.aspx">
+  Nombre<input type="text" name="nombre"/></br>	
+  Apellido<input type="text" name="apellido"/></br>
+  <input type="submit" name="Enviar a C#">
+</form>
